ebe1f8eefc95351a5d9dd3728788bb233efce437
Sebastian Triana martinez <84682654+sebastiantrianamartinez@users.noreply.github.com>
2023-06-23 02:15:38 -0500

    Update search.php

--- endpoints/search.php
+++ endpoints/search.php
@@ -1,1 +1,17 @@
+<?php
+  define("ROOT", dirname(__FILE__, 2));
+  require_once ROOT .'/core/lib/responser.php';
+  require_once ROOT .'/core/search/search.php';
+  
+  $httpRequestData = (isset($_GET["name"])) ? $_POST : json_decode(file_get_contents('php://input'), true);
 
+  $search = new search;
+
+  $searchRequest = $search->search();
+  if($searchRequest["status"] == 200){
+    responser::preformedHttpResponse($httpRequestData["name"]);
+  }
+  else{
+    responser::httpResponse(400, "We didn't found matches, please verify the data", NULL);
+  }
+?>
