e4e283ea1199824f3e7779f65a2c6d60737155cb
Sebastian Triana martinez <84682654+sebastiantrianamartinez@users.noreply.github.com>
2023-06-23 02:20:51 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,14 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Search</title>
+</head>
+<body>
+    <form action="endpoints/search.php" method="GET">
+        <input type="test" name="name" placeholder="Name to search">   
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
