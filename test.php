9b5e7564ce1047b6809790492ce96ac5e58e1706
Sebastian Triana martinez <84682654+sebastiantrianamartinez@users.noreply.github.com>
2023-06-23 02:39:06 -0500

    Add files via upload

--- test.php
+++ test.php
@@ -4,11 +4,52 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search</title>
+    <style>
+        *{
+            margin: 0;
+            padding: 0;
+            font-family: 'Arial';
+        }
+        .container{
+            padding: 20px;
+            background-color: #eee;
+            border-radius: 8px;
+            width: max-content;
+            height: max-content;
+            margin-left: 30px;
+            margin-top: 10px;
+        }
+        h1{
+            font-size: 20px;
+        }
+        input{
+            background-color: transparent;
+            font-size: 14px;
+            padding: 10px 5px;
+            border: 1px solid #1C59EB;
+            border-radius: 5px;
+        }
+        .input-submit{
+            border: none;
+            background-color: #1C59EB;
+            color: #fff;
+            padding: 10px;
+        }
+        p{
+            color: #aaa;
+            margin-left: 30px;
+            margin-top: 5px;
+        }
+    </style>
 </head>
 <body>
-    <form action="endpoints/search.php" method="GET">
-        <input type="test" name="name" placeholder="Name to search">   
-        <input type="submit" value="Enviar">
-    </form>
+    <div class="container">
+        <h1>Buscar:</h1>
+        <form action="endpoints/search.php" method="GET">
+            <input type="test" name="name" placeholder="Name to search">   
+            <input type="submit" value="Enviar" class="input-submit">
+        </form>
+    </div>
+    <p>By Sebastian Triana Martinez</p>
 </body>
 </html>
