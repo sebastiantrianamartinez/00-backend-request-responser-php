6c15c8ecfae41567d0c33c5b10b0fc6da770be29
Sebastian Triana martinez <84682654+sebastiantrianamartinez@users.noreply.github.com>
2023-06-23 02:39:55 -0500

    Add files via upload

--- core/poo-test/search.php
+++ core/poo-test/search.php
@@ -1,19 +1,21 @@
 <?php
   define("ROOT", dirname(__FILE__, 3));
   require_once ROOT .'/core/lib/responser.php';
-  class search{
-    private $array;
-    public function __construct(){
-      $this->array = ["sebastian", "juan", "sofia", "pedro", "luis"];
-    }
 
-    public function search($name){
-      foreach($this->array as $keys => $listName){
-        if($listName === $name){
-          return responser::systemResponse(200, "Matches were found", ["id" => $keys]);
-        }
-        return responser::systemResponse(204, "Matches not found", NULL);
+  class Search {
+      private $array;
+
+      public function __construct() {
+          $this->array = ["sebastian", "juan", "sofia", "pedro", "luis"];
       }
-    }
-    
+
+      public function search($name) {
+          foreach ($this->array as $key => $listName) {
+              if ($listName == $name) {
+                  return responser::systemResponse(200, "Matches were found", ["id" => $key]);
+              }
+          }
+
+          return responser::systemResponse(204, "Matches not found", NULL);
+      }
   }
