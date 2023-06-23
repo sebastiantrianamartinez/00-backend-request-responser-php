6ec239c50c44d41cb5588750e06ddae8175b3c02
Sebastian Triana martinez <84682654+sebastiantrianamartinez@users.noreply.github.com>
2023-06-23 02:39:29 -0500

    Add files via upload

--- endpoints/search.php
+++ endpoints/search.php
@@ -1,15 +1,15 @@
 <?php
   define("ROOT", dirname(__FILE__, 2));
   require_once ROOT .'/core/lib/responser.php';
-  require_once ROOT .'/core/search/search.php';
+  require_once ROOT .'/core/poo-test/search.php';
   
-  $httpRequestData = (isset($_GET["name"])) ? $_POST : json_decode(file_get_contents('php://input'), true);
+  $httpRequestData = (isset($_GET["name"])) ? $_GET : json_decode(file_get_contents('php://input'), true);
 
   $search = new search;
 
-  $searchRequest = $search->search();
+  $searchRequest = $search->search($httpRequestData["name"]);
   if($searchRequest["status"] == 200){
-    responser::preformedHttpResponse($httpRequestData["name"]);
+    responser::preformedHttpResponse($searchRequest);
   }
   else{
     responser::httpResponse(400, "We didn't found matches, please verify the data", NULL);
