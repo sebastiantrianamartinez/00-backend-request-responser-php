3c456f18cda6c402581db143b01b8d586800b8dd
Sebastian Triana martinez <84682654+sebastiantrianamartinez@users.noreply.github.com>
2023-06-23 02:09:08 -0500

    Update search.php

--- core/poo-test/search.php
+++ core/poo-test/search.php
@@ -1,1 +1,19 @@
+<?php
+  define("ROOT", dirname(__FILE__, 3));
+  require_once ROOT .'/core/lib/responser.php';
+  class search{
+    private $array;
+    public function __construct(){
+      $this->array = ["sebastian", "juan", "sofia", "pedro", "luis"];
+    }
 
+    public function search($name){
+      foreach($this->array as $keys => $listName){
+        if($listName === $name){
+          return responser::systemResponse(200, "Matches were found", ["id" => $keys]);
+        }
+        return responser::systemResponse(204, "Matches not found", NULL);
+      }
+    }
+    
+  }
